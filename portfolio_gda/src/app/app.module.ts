82ec6856f6603fefc47b8faacd39a768ae2eae11
Guillermo <guillermo.arena@gmail.com>
2022-09-01 22:26:23 -0300

    creando componentes

--- portfolio_gda/src/app/app.module.ts
+++ portfolio_gda/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HysComponent } from './components/hys/hys.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    HomeComponent,
+    HysComponent,
+    LoginComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
